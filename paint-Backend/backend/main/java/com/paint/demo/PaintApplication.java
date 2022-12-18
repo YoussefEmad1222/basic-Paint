71a137ba2772f6109307a5a393d47f6b0358399e
YoussefEmad1222 <96549295+YoussefEmad1222@users.noreply.github.com>
2022-12-18 23:48:07 +0200

    Add files via upload

---
+++ paint-Backend/backend/main/java/com/paint/demo/PaintApplication.java
@@ -0,0 +1,13 @@
+package com.paint.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaintApplication {
+    
+	public static void main(String[] args) throws CloneNotSupportedException, Exception {
+		SpringApplication.run(PaintApplication.class, args);
+	}
+
+}
